b91f1f7d79a7301772274834a214e1cdbe90c9ce
김승태 <tiga1207@naver.com>
2025-04-07 10:30:42 +0900

    [FEAT] Visual Studio
    At First, Visual Studio Settings to make Console Project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        //1. 객체지향
+        //2. 자료구조
+        //3. 알고리즘
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
